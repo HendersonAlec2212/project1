f7534002e8e9c38485cff9ab375cb2dfb1911a38
Alec <88725250+HendersonAlec2212@users.noreply.github.com>
2021-10-19 22:25:14 -0500

    deleted test api_keys file for future fix

--- api_keys.py
+++
@@ -1,3 +0,0 @@
-# This file will serve as a potential space to store the API Key - There is a method of preventing the config file from being uploaded via the Git Push, it is in the PART 1 of the API homework.
-
-potential_key = ''
